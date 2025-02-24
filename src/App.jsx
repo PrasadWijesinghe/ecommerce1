8622c55e6b567a8aab34c0072d67200eb60f6af6
Prasad Wijesinghe <wmprasadwijesinghe@gmail.com>
2025-02-24 20:39:29 +0530

    commi9t

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Collection from './pages/Collection'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
 
       <Routes>
-        <Route path='/' element={Home} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/Collection' element={<Collection/>} />
+        <Route path='/About' element={<About/>} />
+        <Route path='/Contact' element={<Contact/>} />
+        <Route path='/Product' element={<Product/>} />
+        
       </Routes>
       
     </div>
