2be07f7e3480fb10deb1573ff1ba38038658bf19
Prasad Wijesinghe <wmprasadwijesinghe@gmail.com>
2025-02-24 20:39:57 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+
 
 const App = () => {
   return (
