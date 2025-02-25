8e51e039c55ff6df11c2d403dcaaa1d4041c0351
Prasad Wijesinghe <wmprasadwijesinghe@gmail.com>
2025-02-25 21:30:37 +0530

    com

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { use } from 'react'
 import { assets } from '../assets/frontend_assets/assets' // Ensure correct path
 import { NavLink, Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Navbar = () => {
+
+    const [visible,setvisible] = useState(false);
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
@@ -57,10 +61,18 @@ const Navbar = () => {
             3 {/* Replace 3 with actual cart count dynamically */}
           </p>
         </Link>
-        <img src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="Menu" />
+        <img onClick={()=>setvisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="Menu" />
 
       </div>
 
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full':'w-0'}`}></div>
+
+            <div className='flex flex-col text-gray-600'>
+              <div className='flex items-center gap-4 p-3'>
+                <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="Dropdown" />
+                <p>Back</p>
+              </div>
+            </div>
     </div>
   )
 }
