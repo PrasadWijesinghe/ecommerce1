6325a2cfd96dc45f310ffb02c4975c43c19614d8
Prasad Wijesinghe <wmprasadwijesinghe@gmail.com>
2025-02-25 18:31:59 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,28 @@ import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
+import Orders from './pages/Orders'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
 
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Collection' element={<Collection/>} />
         <Route path='/About' element={<About/>} />
         <Route path='/Contact' element={<Contact/>} />
-        <Route path='/Product' element={<Product/>} />
+        <Route path='/Product/:ProductId' element={<Product/>} />
+        <Route path='/Orders' element={<Orders/>} />
+        <Route path='/Cart' element={<Cart/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/PlaceOrder' element={<PlaceOrder/>} />
         
       </Routes>
       
