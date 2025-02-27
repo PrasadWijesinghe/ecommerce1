e91d2ddea51eed8b31294d58c4bfc455074db647
Prasad Wijesinghe <wmprasadwijesinghe@gmail.com>
2025-02-27 20:34:38 +0530

    commit

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { products } from '../assets/frontend_assets/assets'
+import { ShopContext } from '../context/shopContext'
 
 const Collection = () => {
+
+    const { product }=useContext(ShopContext);
+
   return (
-    <div>
+    
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+        {products.map(product => (
+          <div key={product.id} className='border border-gray-200 rounded-lg overflow-hidden'>
+            <img src={product.image} alt={product.title} className='w-full h-60 object-cover' />
+            <div className='p-4'>
+              <p className='font-bold'>{product.title}</p>
+              <p className='font-bold'>{product.price}</p>
+            </div>
+          </div>
+        ))}
       
     </div>
   )
