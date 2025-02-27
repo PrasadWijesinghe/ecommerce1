7eabdef64cfedd3281701f16de8235a447425754
Prasad Wijesinghe <wmprasadwijesinghe@gmail.com>
2025-02-27 19:47:27 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <div className='flex items-center justify-between py-5 font-medium relative'>
 
             {/* Logo */}
-            <img src={assets.logo} className='w-36' alt="Logo" />
+            <Link to='/'><img src={assets.logo} className='w-36' alt="Logo" /></Link>
 
             {/* Desktop Navigation */}
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import { Link } from 'react-router-dom';
 
+const ProductItem = ({ id, image, name, price }) => {
+    const { currency } = useContext(ShopContext); // Fixed typo in 'currency'
 
-const ProductItem = ({id,image,name,price}) => {
+    return (
+        <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
+            <div className='overflow-hidden'> {/* Fixed typo in Tailwind class */}
+                <img 
+                    className='hover:scale-110 transition ease-in-out' 
+                    src={Array.isArray(image) ? image[0] : image} // Handle both array and string cases
+                    alt={name} 
+                />
+            </div>
+            <p className='pt-3 pb-1 text-sm'>{name}</p>
+            <p className='text-sm font-medium'>{currency}{price}</p> {/* Fixed currency variable */}
+        </Link>
+    );
+};
 
-    const {currunecy} = useContext(ShopContext);
-
-
-  return (
-    <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
-
-        <div className='overflow-hiddeen'>
-            <img className='hover:scale-110 transition ease-in-out ' src={image[0]} alt="" />
-        </div>
-        <p className='pt-3 pb-1 text-sm'>{name}</p>
-        <p className='text-sm font-medium'>{currunecy}{price}</p>
-
-    </Link>
-  )
-}
-
-export default ProductItem
+export default ProductItem;
