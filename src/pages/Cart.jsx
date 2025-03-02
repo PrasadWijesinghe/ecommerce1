26e3169a6b4ee93596dbce4299dcae5c191b4cc8
Prasad Wijesinghe <wmprasadwijesinghe@gmail.com>
2025-03-02 20:21:47 +0530

    commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import Title from '../components/Title';
+import { ShopContext } from '../context/ShopContext'; // Ensure this is the correct path
 
 const Cart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { products, currency, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
 
-export default Cart
+  useEffect(() => {
+    const tempData = [];
+
+    for (const item in cartItems) {
+      for (const size in cartItems[item]) {
+        if (cartItems[item][size] > 0) {
+          tempData.push({
+            _id: item,
+            size: size,
+            quantity: cartItems[item][size],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className='border-t pt-14'>
+      <div className='text-2xl mb-3'>
+        <Title text1={'YOUR'} text2={'CART'} />
+      </div>
+
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find((product) => product._id === item._id);
+
+          return (
+            <div
+              key={index}
+              className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'
+            >
+              <div className='flex items-start gap-6'>
+                {productData && (
+                  <img src={productData.image} alt={productData.name} className='w-16 sm:w-20' />
+                )}
+                <div>
+                  <p className='text-xs sm:text-lg font-medium'>{productData?.name}</p>
+                  <div className='flex items-center gap-5 mt-2'>
+                    <p>
+                      {currency}
+                      {productData?.price}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
