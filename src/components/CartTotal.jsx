480fc1bd41afbe7e7384305021b111d32b87f619
Prasad Wijesinghe <wmprasadwijesinghe@gmail.com>
2025-03-04 20:25:26 +0530

    commit

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
 
 const CartTotal = () => {
+  const { currency, delivery_fee, getCartAmount } = useContext(ShopContext);
 
+  const cartTotal = getCartAmount(); // Store the result once to avoid multiple function calls
 
-    
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="w-full">
+      <div className="text-2xl">
+        <Title text1={'CART'} text2={'TOTAL'} />
+      </div>
 
-export default CartTotal
+      <div className="flex flex-col gap-2 mt-2 text-sm">
+        <div className="flex justify-between">
+          <p>Subtotal</p>
+          <p>{currency} {cartTotal}.00</p>
+        </div>
+        <hr />
+        <div className="flex justify-between">
+          <p>Shipping Fee</p>
+          <p>{currency} {cartTotal === 0 ? 0 : delivery_fee}</p>
+        </div>
+        <hr />
+        <div className="flex justify-between">
+          <b>Total</b>
+          <b>{currency} {cartTotal === 0 ? 0 : cartTotal + delivery_fee}</b>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartTotal;
